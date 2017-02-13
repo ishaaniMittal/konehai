2c595b5b24c55e6a7af1a15b754bf77b9b5d9811
imittal <ishaani.94@gmail.com>
2017-02-13 21:51:41 +0530

    first commit

---
+++ src/main/java/com/konehai/KoneHaiApplication.java
@@ -0,0 +1,12 @@
+package com.konehai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoneHaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoneHaiApplication.class, args);
+	}
+}
